9f834302a5cd1d4d28fa96ef4847ad819eeeb367
n23020 <n23020@std.it-college.ac.jp>
2023-12-13 07:50:35 +0000

    add change index.js
    index.jsを変更した

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
+import Header from '../components/header'
+import Hero from '../components/hero'
+import Fooder from '../components/footer'
+
 const Home = () => {
   return (
     <>
-      <header>HEADER</header>
+      <Header />
 
       <main>
-        <div>
-          <h1>CUBE</h1>
-          <p>アウトプットしていくサイト</p>
-        </div>
+        <Hero />
       </main>
 
-      <footer>FOOTER</footer>
+      <Fooder />
     </>
   )
 }
